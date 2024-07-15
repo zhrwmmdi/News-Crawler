3cd968faa94426ced9c2bc8fde8f2ef253fcd00d
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-07-15 11:21:42 +0330

    Models created

---
+++ models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from peewee import (
+    Model, SqliteDatabase, CharField,
+    TextField, ForeignKeyField, BooleanField, DateTimeField)
+
+database = SqliteDatabase('Posts.db')
+
+
+class BaseModel(Model):
+    created_time = DateTimeField(default=datetime.now())
+
+    class Meta:
+        """
+        the Meta class is a special inner class used to
+        provide configuration and metadata about the model
+        """
+        database = database
+
+
+class Category(BaseModel):
+    name = CharField()
+
+
+class Article(BaseModel):
+    url = CharField()
+    title = CharField(null=True)
+    body = TextField(null=True)
+    category = ForeignKeyField(Category, backref='articles')
+    is_completed = BooleanField(default=False)
