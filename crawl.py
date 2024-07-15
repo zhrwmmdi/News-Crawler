120ff50f0723b5ac211734b4f7ce26c0ecf8e2a0
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-07-15 15:36:08 +0330

    pep8 rules done

--- crawl.py
+++ crawl.py
@@ -6,7 +6,7 @@ def crawl_page_by_url(url):
     response = requests.get(url)
     if response.status_code == 200:
         html_doc = response.text
-        soup = BeautifulSoup(html_doc,'html.parser')
+        soup = BeautifulSoup(html_doc, 'html.parser')
         title = soup.find('h1', attrs={'class': 'news-title'})
         if title:
             print(title.text)
@@ -16,7 +16,9 @@ def get_all_links(topic, page_count=10):
     link_tags = list()
 
     for i in range(page_count):
-        response = requests.get(f'https://www.trthaber.com/haber/dunya/{i}.sayfa.html')
+        response = requests.get(
+            f'https://www.trthaber.com/haber/dunya/{i}.sayfa.html'
+        )
         soup = BeautifulSoup(response.text, 'html.parser')
         ll = soup.find_all('a')
         link_tags.extend(ll)
@@ -30,7 +32,7 @@ def get_all_links(topic, page_count=10):
 def validate_links(link_hrefs):
     valid_links = list()
     for link in link_hrefs:
-        if link.endswith('html') and link.startswith('https://www.trthaber.com/haber/dunya/'):
+        if (link.endswith('html') and
+                link.startswith('https://www.trthaber.com/haber/dunya/')):
             valid_links.append(link)
     return valid_links
-
